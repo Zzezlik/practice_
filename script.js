6c11d060b48f4f39df5847b4e0661182459bd220
zzezl <maxmandarin29@gmail.com>
2025-10-28 22:07:30 +0200

    ?

--- script.js
+++ script.js
@@ -8,4 +8,11 @@ questions.addEventListener("click", e => {
     console.dir(e.target)
     const parag = e.target.firstElementChild
     console.dir(parag)
+    if (e.target.tagName !== "LI") return
+    parag.classList.toggle("hidden")
+    console.dir(parag.classList.contains("hidden"))
+    e.target.style.backgroundImage = parag.classList.contains("hidden")
+        ? "url('./imgs/assets/arrow-in.png')"
+        : "url('./imgs/assets/arrow-out.png')"
+
 })
